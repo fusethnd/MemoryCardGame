44edecb43db7cc989753662d731a6c985bb38b09
fusethnd <fusethnd@gmail.com>
2024-02-02 09:50:16 +0700

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Thanadon Boontawee on 2/2/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
